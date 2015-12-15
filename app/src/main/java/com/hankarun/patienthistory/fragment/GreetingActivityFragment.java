0a6a21b5d919764eabdaa559a26ede3f42b0d3a0
hankarun <hankarun@gmail.com>
2015-12-15 09:56:37 +0430

    add animation
    Greeting screen welcome animation.

--- app/src/main/java/com/hankarun/patienthistory/fragment/GreetingActivityFragment.java
+++ app/src/main/java/com/hankarun/patienthistory/fragment/GreetingActivityFragment.java
@@ -6,14 +6,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.hankarun.patienthistory.R;
 import com.hankarun.patienthistory.activity.QuestionsActivity;
 
-public class GreetingActivityFragment extends Fragment implements Button.OnClickListener {
+public class GreetingActivityFragment extends Fragment implements Button.OnClickListener, Animation.AnimationListener {
+    TextView txtMessage;
+    TextView mainMessage;
+    Button button;
+    Button begin;
 
-    //private SimpleCursorAdapter dataAdapter;
+    // Animation
+    Animation animFadein;
+    Animation animFadein1;
+    Animation animFadein2;
+    Animation animFadeout;
 
     public GreetingActivityFragment() {
     }
@@ -22,8 +34,38 @@ public class GreetingActivityFragment extends Fragment implements Button.OnClick
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_greeting, container, false);
-        Button button = (Button) rootView.findViewById(R.id.startButton);
+        button = (Button) rootView.findViewById(R.id.startButton);
         button.setOnClickListener(this);
+
+        txtMessage = (TextView) rootView.findViewById(R.id.greetingText);
+        mainMessage = (TextView) rootView.findViewById(R.id.mainText);
+        begin = (Button) rootView.findViewById(R.id.beginButton);
+
+        animFadein = AnimationUtils.loadAnimation(getContext(),
+                R.anim.fade_in_move);
+
+        animFadein1 = AnimationUtils.loadAnimation(getContext(),
+                R.anim.fade_in);
+
+        animFadein2 = AnimationUtils.loadAnimation(getContext(),
+                R.anim.fade_in);
+
+        animFadeout = AnimationUtils.loadAnimation(getContext(),
+                R.anim.fade_out);
+
+        animFadein.setAnimationListener(this);
+        animFadein1.setAnimationListener(this);
+        animFadein2.setAnimationListener(this);
+        animFadeout.setAnimationListener(this);
+
+        begin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                begin.startAnimation(animFadeout);
+            }
+        });
+
+
         return rootView;
     }
 
@@ -33,87 +75,28 @@ public class GreetingActivityFragment extends Fragment implements Button.OnClick
         startActivity(intent);
     }
 
-    /*public void populateList1(){
-        String[] columns = new String[] {
-                QuesSQLiteHelper.GROUP_TABLE_ID,
-                QuesSQLiteHelper.GROUP_TABLE_TEXT,
-                QuesSQLiteHelper.GROUP_TABLE_DETAIL
-        };
+    @Override
+    public void onAnimationStart(Animation animation) {
 
-        int[] to = new int[] {
-                R.id.textView,
-                R.id.textView2,
-                R.id.textView3,
-        };
-
-        dataAdapter = new SimpleCursorAdapter(
-                getActivity(),
-                R.layout.item,
-                null,
-                columns,
-                to,
-                0);
-
-        list.setAdapter(dataAdapter);
-        //Ensures a loader is initialized and active.
-        getLoaderManager().initLoader(0, null, this);
-    }
-
-    public void populateList(){
-        String[] columns = new String[] {
-                QuesSQLiteHelper.QUESTION_TABLE_ID,
-                QuesSQLiteHelper.QUESTION_TABLE_TEXT,
-                QuesSQLiteHelper.QUESTION_TABLE_GROUPID
-        };
-
-        int[] to = new int[] {
-                R.id.textView,
-                R.id.textView2,
-                R.id.textView3,
-        };
-
-        dataAdapter = new SimpleCursorAdapter(
-                getActivity(),
-                R.layout.item,
-                null,
-                columns,
-                to,
-                0);
-
-        list.setAdapter(dataAdapter);
-        //Ensures a loader is initialized and active.
-        Bundle b = new Bundle();
-        b.putString("test","test");
-        getLoaderManager().initLoader(5, b, this);
     }
 
     @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        Log.d(args.getString("test"),"-");
-        /*String[] projection = {
-                QuesSQLiteHelper.GROUP_TABLE_ID,
-                QuesSQLiteHelper.GROUP_TABLE_TEXT,
-                QuesSQLiteHelper.GROUP_TABLE_DETAIL};
-        CursorLoader cursorLoader = new CursorLoader(getActivity(),
-                DataContentProvider.CONTENT_URI2, projection, null, null, null);
-        String[] projection = {
-                QuesSQLiteHelper.QUESTION_TABLE_ID,
-                QuesSQLiteHelper.QUESTION_TABLE_TEXT,
-                QuesSQLiteHelper.QUESTION_TABLE_TYPE,
-                QuesSQLiteHelper.QUESTION_TABLE_GROUPID};
-        Uri uri = Uri.parse(DataContentProvider.CONTENT_URI2 + "/" + id);
-        CursorLoader cursorLoader = new CursorLoader(getActivity(),
-                uri, projection, null, null, null);
-        return cursorLoader;
+    public void onAnimationEnd(Animation animation) {
+        if (animation == animFadein) {
+            mainMessage.startAnimation(animFadein1);
+        }
+        if (animation == animFadein1) {
+            button.startAnimation(animFadein2);
+        }
+        if (animation == animFadeout) {
+            begin.clearAnimation();
+            begin.setVisibility(View.GONE);
+            txtMessage.startAnimation(animFadein);
+        }
     }
 
     @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        dataAdapter.swapCursor(data);
-    }
+    public void onAnimationRepeat(Animation animation) {
 
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-        dataAdapter.swapCursor(null);
-    }*/
+    }
 }
